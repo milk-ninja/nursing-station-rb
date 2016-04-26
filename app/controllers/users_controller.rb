69765d012b43c631921d1104b496d2b64b2f9572
Jennifer Ford <jenncford@gmail.com>
2016-04-26 15:08:27 -0400

    adding users controller with login in meth

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  before_action :authenticate!, only: [:check_auth]
+
+ def login
+    @user = User.find_by!(first_name: params["first_name"])
+    if @user.authenticate(params[:password])
+      render json: { user: @user.as_json(only:[:first_name, :auth_token]) },
+             status: :ok
+    else
+      render json: { message: "Username or password could not be found." },
+             status: :unauthorized
+    end
+  end
+
+  def check_auth
+    if current_user
+      render json: { message: "Logged in as #{current_user.first_name}" },
+             status: :ok
+    else
+      render json: { message: "Auth isn't working yet." },
+              status: :unauthorized
+    end
+  end
+end
