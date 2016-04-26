39d6317d18009c1d354502898eae5f291152faf2
Jennifer Ford <jenncford@gmail.com>
2016-04-26 09:47:28 -0400

    edited validations for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,26 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  validates :password, format: {
-              with: /.\w{4,15}/, message: "Must be between 4 and 15 characters"
+  validates :password, presence: true, uniqueness: true, length: { in: 6..20, 
+                  message: "Must be between 4 and 15 characters"
   }
-  validate :email, format: {
+  validate :email, presence: true, uniqueness: true, format: {
             with: /.+@.+\..+/, message: "Please put in valid email"
   }
   validates_presence_of :first_name, :last_name, :email, :password
+
+  def ensure_auth_token
+    unless self.auth_token
+      self.auth_token = User.generate_token
+    end
+  end
+
+  def self.generate_token
+    token = SecureRandom.hex
+    while User.exists?(auth_token: token)
+      token = SecureRandom.hex
+    end
+    token
+  end
+
 end
