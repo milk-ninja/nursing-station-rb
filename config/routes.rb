ce1e1f045b824c0f1ae08b6f63d954648cb15740
Jennifer Ford <jenncford@gmail.com>
2016-04-26 15:05:52 -0400

    got routes to show up login controller should work

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
   post "/register", to: "registrations#create"
+
+  post "/login", to: "users#login"
   # Example resource route with options:
   #   resources :products do
   #     member do
