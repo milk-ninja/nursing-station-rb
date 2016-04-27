3855c4dd61ab9d3501188194505203c1aab7fbed
Jennifer Ford <jenncford@gmail.com>
2016-04-27 10:43:47 -0400

    Updated users model with avatar associations and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ActiveRecord::Base
             with: /.+@.+\..+/, message: "Please put in valid email"
   }
   validates_presence_of :first_name, :last_name, :email, :password
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
   def ensure_auth_token
     unless self.auth_token
