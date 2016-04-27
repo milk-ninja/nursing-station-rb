cdc021053bdc7e19e8e3a716f4122c75fab02326
Jennifer Ford <jenncford@gmail.com>
2016-04-27 11:54:12 -0400

    Testing to see if works with out valdations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ActiveRecord::Base
   has_secure_password
-  validates :password, presence: true, uniqueness: true, length: { in: 6..20,
-                  message: "Must be between 4 and 15 characters"
-  }
-  validates :email, presence: true, uniqueness: true, format: {
-            with: /.+@.+\..+/, message: "Please put in valid email"
-  }
-  validates_presence_of :first_name, :last_name, :email, :password
+  # validates :password, presence: true, uniqueness: true, length: { in: 6..20,
+  #                 message: "Must be between 4 and 15 characters"
+  # }
+  # validates :email, presence: true, uniqueness: true, format: {
+  #           with: /.+@.+\..+/, message: "Please put in valid email"
+  # }
+  # validates_presence_of :first_name, :last_name, :email, :password
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
