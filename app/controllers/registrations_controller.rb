577c92ab3e200995aa03c1fa87f18788809028e5
Jennifer Ford <jenncford@gmail.com>
2016-04-27 12:18:45 -0400

    Updated validation and user model

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,7 +4,6 @@ class RegistrationsController < ApplicationController
     @user.ensure_auth_token
     if @user.save
       render "create.json.jbuilder", status: :created
-    
     else
       render json: { errors: @user.errors.full_messages },
              status: :unprocessable_entity
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,13 @@
 class User < ActiveRecord::Base
   has_secure_password
-  # validates :password, presence: true, uniqueness: true, length: { in: 6..20,
-  #                 message: "Must be between 4 and 15 characters"
-  # }
-  # validates :email, presence: true, uniqueness: true, format: {
-  #           with: /.+@.+\..+/, message: "Please put in valid email"
-  # }
-  # validates_presence_of :first_name, :last_name, :email, :password
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+
+  has_attached_file :avatar
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+
+  validates :email, presence: true, uniqueness: true, format: {
+            with: /.+@.+\..+/, message: "Please put in valid email"
+  }
+  validates_presence_of :first_name, :last_name, :email
 
   def ensure_auth_token
     unless self.auth_token
@@ -21,6 +20,6 @@ class User < ActiveRecord::Base
     while User.exists?(auth_token: token)
       token = SecureRandom.hex
     end
-      token
+    token
   end
 end
