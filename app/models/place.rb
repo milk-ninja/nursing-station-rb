1f154f52ad0a334a9cff9b8e539e60e370552a6d
Jennifer Ford <jenncford@gmail.com>
2016-05-02 15:56:36 -0400

    updated the place model

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,5 @@
 class Place < ActiveRecord::Base
   validates :street, presence: true, uniqueness: true
-  validates_presence_of :name, :city, :state, :zip
+  validates_presence_of :name, :city, :state, :zip, :lat, :long
   has_many :ratings
 end
