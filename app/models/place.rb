e1e18c0697f4cd2c3f55dca4bac05edc19641616
Jennifer Ford <jenncford@gmail.com>
2016-05-03 11:27:06 -0400

    fixed Model validations and geocoder

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,4 +2,17 @@ class Place < ActiveRecord::Base
   validates :street, presence: true, uniqueness: true
   validates_presence_of :name, :city, :state, :zip, :lat, :long
   has_many :ratings
+
+  geocoded_by :full_address, :latitude  => :lat, :longitude => :long
+  before_validation :ensure_coords!
+
+  def ensure_coords!
+    unless self.lat && self.long
+      self.geocode
+    end
+  end
+
+  def full_address
+    [street, city, state, zip].compact.join(", ")
+  end
 end
