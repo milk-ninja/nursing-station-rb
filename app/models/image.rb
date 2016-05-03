427ea9100b73c159daef5594c903e88918a3376f
Jennifer Ford <jenncford@gmail.com>
2016-05-03 17:31:06 -0400

    Paperclip required validations for image to work

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,5 +2,7 @@ class Image < ActiveRecord::Base
   belongs_to :rating
 
   has_attached_file :image
-  ## TODO: Add validation for paperclip images
+
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  validates_attachment_file_name :image, matches: [/png\Z/, /jpe?g\Z/]
 end
