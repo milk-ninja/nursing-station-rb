5786c177b84a93a1ee88c5a474d0046cf61a31e6
Jennifer Ford <jenncford@gmail.com>
2016-05-04 15:51:19 -0400

    spacing

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -4,7 +4,6 @@ class PlacesController < ApplicationController
   def create
     @place = Place.new(place_params)
     @rating = @place.ratings.new(rating_params)
-
     if @place.save
       render "create.json.jbuilder", status: :created
     else
