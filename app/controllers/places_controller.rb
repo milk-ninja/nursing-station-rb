ea2ba245bc19db594aed3fad16e15261074d1ba7
Jennifer Ford <jenncford@gmail.com>
2016-05-10 16:33:55 -0400

    work on making it work for postman babies r us of the us here you come

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,15 +1,22 @@
 class PlacesController < ApplicationController
   before_action :authenticate!, only: [:check_auth]
-  def spots(name, type)
+
+  def spots(name, type, radius)
     @client = GooglePlaces::Client.new(ENV["PLACES_API_KEY"])
-    a = @client.spots(33.756888, -84.3918611, :name => 'BuyBuy Baby', :type => 'store', :radius => 120000)
-    new_places = a.map do |x|
+    a = @client.spots(33.756888, -84.3918611, :name => name.to_s, :type => type.to_s, :radius => radius.to_i)
+    new_places = a.reject {|place| place.name.length = name.length}
+    new_places.map do |x|
       name = x.name
       street = x.vicinity.split(",").first
       city = x.vicinity.split(",").last
       avatar = x.photos[0].fetch_url(800)
-      place = Place.new(name: name, street: street, city: city, state: "Ga", lat: x.lat, lng: x.lng, avatar: avatar)
-      place.save
+      @place = Place.new(name: name, street: street, city: city, state: "Ga", lat: x.lat, lng: x.lng, avatar: avatar)
+      if @place.save
+        render "create.json.jbuilder", status: :created
+      else
+        render json: {errors: place.errors.full_messages},
+        status: :inprocessable_entity
+      end
     end
 
   end
@@ -36,7 +43,7 @@ class PlacesController < ApplicationController
   end
 
   def find_nearby
-    @places = Place.near([params[:lat], params[:lng]], 20)
+    @places = Place.near([params[:lat], params[:lng]], 30)
     render "find_nearby.json.jbuilder"
   end
 
