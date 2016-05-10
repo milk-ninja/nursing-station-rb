112e9d7b39c89dc37990b7e075193db1a4caefcc
Jennifer Ford <jenncford@gmail.com>
2016-05-10 19:35:31 -0400

    Added Admin rights to users table later versions controll of data on site

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160504195225) do
+ActiveRecord::Schema.define(version: 20160510232501) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,13 +60,14 @@ ActiveRecord::Schema.define(version: 20160504195225) do
     t.string   "email"
     t.string   "password_digest"
     t.integer  "zipcode"
-    t.datetime "created_at",          null: false
-    t.datetime "updated_at",          null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
     t.string   "auth_token"
     t.string   "avatar_file_name"
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.boolean  "admin",               default: false
   end
 
 end
