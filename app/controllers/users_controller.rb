697c1fba02cf6b3bb285d7d68615a51402b30652
Jennifer Ford <jenncford@gmail.com>
2016-05-10 14:58:43 -0400

    fucking merge conflict decided to show up got pushed to heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,15 +2,9 @@ class UsersController < ApplicationController
   before_action :authenticate!, only: [:check_auth]
 
  def login
-<<<<<<< HEAD
-    @user = User.find_by!(first_name: params["first_name"])
-    if @user.authenticate(params[:password])
-      render json: { user: @user.as_json(only:[:first_name, :auth_token, :id]) },
-=======
     @user = User.find_by!(username: params["username"])
     if @user.authenticate(params[:password])
       render "login.json.jbuilder",
->>>>>>> 53c7bdbdabe9d5f212e0e8a303b1e80f09156319
              status: :ok
     else
       render json: { message: "Username or password could not be found." },
@@ -24,11 +18,7 @@ class UsersController < ApplicationController
              status: :ok
     else
       render json: { message: "Auth isn't working yet." },
-<<<<<<< HEAD
-              status: :unauthorized
-=======
              status: :unauthorized
->>>>>>> 53c7bdbdabe9d5f212e0e8a303b1e80f09156319
     end
   end
 end
