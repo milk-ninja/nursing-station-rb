8ced32278176f9da88a73f17f01b9e11500062c1
Jennifer Ford <jenncford@gmail.com>
2016-05-10 19:54:12 -0400

    Commented out email stuff

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,8 +4,8 @@ class RegistrationsController < ApplicationController
     @user.ensure_auth_token
     if @user.save
       render "create.json.jbuilder", status: :created
-      mail = WelcomeMailer.welcome(@user)
-      mail.deliver_now
+      # mail = WelcomeMailer.welcome(@user)
+      # mail.deliver_now
     else
       render json: { errors: @user.errors.full_messages },
              status: :unprocessable_entity
@@ -13,6 +13,6 @@ class RegistrationsController < ApplicationController
   end
   private
   def user_params
-    params.permit(:first_name, :last_name, :username, :email, :password, :zipcode, :avatar, :admin)
+    params.permit(:first_name, :last_name, :username, :email, :password, :zipcode, :avatar)
   end
 end
